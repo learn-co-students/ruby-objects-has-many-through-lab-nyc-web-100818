a3c78824215afad7a1a4f28c7045b1464fbe5bca
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-10-14 22:24:22 -0400

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,3 +1,9 @@
+# The Appointment model:
+#
+# The Appointment class needs a class variable @@all that begins as an empty array.
+# The Appointment class needs a class method .all that lists each appointment in the class variable.
+# An appointment should be initialized with a date (as a string, like "Monday, August 1st"), a patient, and a doctor. The appointment should be saved in the @@all array.
+
 class Appointment
 
   attr_accessor :patient,:doctor,:date
