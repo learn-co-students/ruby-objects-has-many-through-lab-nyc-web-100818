ad21d73f8461a34186734ee5af92505f83bb78af
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-10-14 22:31:33 -0400

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,13 @@
+# The Doctor model:
+#
+# The Doctor class needs a class variable @@all that begins as an empty array.
+# The Doctor class needs a class method .all that lists each doctor in the class variable.
+# A doctor should be initialized with a name and be saved in the @@all array.
+# The Doctor class needs an instance method, #new_appointment, that takes in a an instance of the Patient class and a date, and creates a new appointment. That appointment should know that it belongs to the doctor
+# The Doctor class needs an instance method, #appointments, that iterates through all appointments and finds those belonging to this doctor.
+# The Doctor class needs an instance method, #patients, that iterates over that doctor's appointments and collects the patient that belongs to each appointment.
+
+
 class Doctor
 
   attr_accessor :name
